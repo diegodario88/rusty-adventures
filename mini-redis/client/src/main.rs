e815a076a6b1dae16a7912c47c235ae2235a7d9c
Diego Dario <diego.dario@gazin.com.br>
2023-04-09 22:05:49 -0300

    init: tokio introduction tutorial

---
+++ mini-redis/client/src/main.rs
@@ -0,0 +1,18 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello");
+
+    println!("Some code without await");
+
+    let resolved_value = result.await?;
+
+    println!("redis key {:?}", resolved_value);
+
+    Ok(())
+}
