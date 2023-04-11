31b85fe398fa7dad6d385a3d652af90746830a2a
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2023-04-11 11:24:46 -0300

    feat(server): add share state across connections

--- tutorials/mini-redis/server/src/main.rs
+++ tutorials/mini-redis/server/src/main.rs
@@ -1,41 +1,48 @@
+use bytes::Bytes;
 use log::{error, info};
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
+
+type InMemoryDB = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
     info!("Starting mini-redis server");
+
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let db: InMemoryDB = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
 
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: InMemoryDB) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db: HashMap<String, Vec<u8>> = HashMap::new();
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 info!("Successfully inserted into {:?}", cmd.key().to_string());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    info!("Getting from {:?}", cmd.key().to_string());
-                    Frame::Bulk(value.clone().into())
+                    info!("Getting from {:?}", cmd.key());
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
