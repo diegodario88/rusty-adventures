16db6c041fd50962cf3feaa0b1692eac15bffbb6
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2023-04-21 17:54:03 -0300

    4300 /5299 XP

--- code-examples/microsoft-training/src/main.rs
+++ code-examples/microsoft-training/src/main.rs
@@ -72,12 +72,40 @@ fn car_factory<'color>(color: &'color String, gears: i8, miles: u32) -> Car<'col
     }
 }
 
+#[derive(Debug)]
+struct Counter {
+    length: usize,
+    count: usize,
+}
+
+impl Counter {
+    fn new(length: usize) -> Counter {
+        Counter { count: 0, length }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count <= self.length {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
+    let counter = Counter::new(10);
+
     let indian_red_color = String::from("IndianRed");
     let rosy_brown = String::from("RosyBrown");
     let salmon = String::from("Salmon");
 
-    for _ in 1..10 {
+    for _ in counter {
         println!("Car made: {:?}", car_factory(&indian_red_color, 12, 35))
     }
 
