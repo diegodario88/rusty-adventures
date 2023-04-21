5dafe53dea796116364bcfce5037761b15fb0c09
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2023-04-21 17:13:45 -0300

    2100 /5299 XP

--- code-examples/microsoft-training/src/main.rs
+++ code-examples/microsoft-training/src/main.rs
@@ -26,8 +26,9 @@ enum Age {
 }
 
 #[derive(PartialEq, Debug)]
-struct Car {
-    color: String,
+struct Car<'color> {
+    color_name: &'color String,
+    color_hex: String,
     motor: Transmission,
     roof: bool,
     mileage: (Age, u32),
@@ -41,13 +42,13 @@ fn car_quality(miles: u32) -> (Age, u32) {
     (Age::New, miles)
 }
 
-fn car_factory(color: String, gears: i8, miles: u32) -> Car {
-    let hex_color: String;
+fn car_factory<'color>(color: &'color String, gears: i8, miles: u32) -> Car<'color> {
+    let color_hex: String;
 
-    if let Some(color_for_sure) = COLORS.get(&color) {
-        hex_color = color_for_sure.to_owned();
+    if let Some(color_for_sure) = COLORS.get(color) {
+        color_hex = color_for_sure.to_owned();
     } else {
-        hex_color = String::from("#FFFAFA");
+        color_hex = String::from("#FFFAFA");
     }
 
     let mut motor = Transmission::Manual;
@@ -63,7 +64,8 @@ fn car_factory(color: String, gears: i8, miles: u32) -> Car {
     }
 
     Car {
-        color: hex_color,
+        color_name: color,
+        color_hex,
         motor,
         roof,
         mileage: car_quality(miles),
@@ -71,16 +73,17 @@ fn car_factory(color: String, gears: i8, miles: u32) -> Car {
 }
 
 fn main() {
+    let indian_red_color = String::from("IndianRed");
+    let rosy_brown = String::from("RosyBrown");
+    let salmon = String::from("Salmon");
+
     for _ in 1..10 {
-        println!(
-            "Car made: {:?}",
-            car_factory(String::from("IndianRed"), 12, 35)
-        )
+        println!("Car made: {:?}", car_factory(&indian_red_color, 12, 35))
     }
 
-    let car1 = car_factory(String::from("Salmon"), 5, 3500);
-    println!("Car made: {:?}", car1);
+    let golf = car_factory(&salmon, -128, 3500);
+    println!("Car made: {:?}", golf);
 
-    let car2 = car_factory(String::from("RosyBrown"), -128, 0);
-    println!("Car made: {:?}", car2);
+    let corola = car_factory(&rosy_brown, 5, 0);
+    println!("Car made: {:?}", corola);
 }
