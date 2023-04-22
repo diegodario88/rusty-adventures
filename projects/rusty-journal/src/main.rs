78828d2395c39649f97008ab64edaa9b89962a61
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2023-04-22 15:41:21 -0300

    2500 /7699 XP

--- projects/rusty-journal/src/main.rs
+++ projects/rusty-journal/src/main.rs
@@ -1,3 +1,22 @@
+use structopt::StructOpt;
+mod cli;
+mod tasks;
+
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
 fn main() {
-    println!("Hello, world!");
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    match action {
+        Add { task } => Task::add_task(journal_file, Task::new(task)),
+        List => Task::list_tasks(journal_file),
+        Done { position } => Task::complete_task(journal_file, position),
+    }
+    .expect("Failed perform  action")
 }
